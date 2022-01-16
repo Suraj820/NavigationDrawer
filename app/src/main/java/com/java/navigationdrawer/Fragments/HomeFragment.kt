eab74a2db8757c3079cb3cf2c19b2f63d18aece8
Suraj820 <king007suraj@gmail.com>
2022-01-16 22:03:18 +0530

    Initial Commit

---
+++ app/src/main/java/com/java/navigationdrawer/Fragments/HomeFragment.kt
@@ -0,0 +1,28 @@
+package com.java.navigationdrawer.Fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
+import com.java.navigationdrawer.R
+
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        view.findViewById<Button>(R.id.button_home)?.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.nav_home_to_content, null)
+        )
+        return view
+    }
+}
